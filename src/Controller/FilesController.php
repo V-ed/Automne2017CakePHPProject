23d3861d242519d620a0065011cab68556ffe470
V-ed <guillaumemarcoux@gmail.com>
2017-10-01 22:32:29 -0400

    Added an ever so slightly graded gray background to home and added fixtures to File edit

--- src/Controller/FilesController.php
+++ src/Controller/FilesController.php
@@ -111,37 +111,58 @@ class FilesController extends AppController
         $file = $this->Files->get($id, [
             'contain' => []
         ]);
+        
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $file = $this->Files->patchEntity($file, $this->request->getData());
-            if ($this->Files->save($file)) {
-                $this->Flash->success(__('The file has been saved.'));
+            
+            
+            if (!empty($this->request->data['name']['name'])) {
                 
-                return $this->redirect(['action' => 'index']);
+                $fileName = $this->request->data['name']['name'];
+                $uploadPath = 'files';
+                $uploadFile = WWW_ROOT . 'img' . DS . $uploadPath . DS . $fileName;
+                
+                if (move_uploaded_file($this->request->data['name']['tmp_name'], $uploadFile)) {
+                    
+                    $file = $this->Files->patchEntity($file, $this->request->getData());
+                    $file->name = $fileName;
+                    $file->path = $uploadPath;
+                    
+                    if ($this->Files->save($file)) {
+                        $this->redirect(['action' => index]);
+                        $this->Flash->success(__('File has been uploaded and modified successfully.'));
+                    } else {
+                        $this->Flash->error(__('The file has been saved but there was a problem saving it in the database. Contact the administrator to seek some help.'));
+                    }
+                    
+                } else {
+                    $this->Flash->error(__('Unable to upload file, please try again.'));
+                }
+            } else {
+                $this->Flash->error(__('Please choose a file to upload.'));
             }
-            $this->Flash->error(__('The file could not be saved. Please, try again.'));
-        }
-        $evidenceItems = $this->Files->EvidenceItems->find('list', ['limit' => 200]);
-        $this->set(compact('file', 'evidenceItems'));
-        $this->set('_serialize', ['file']);
-    }
-    
-    /**
-    * Delete method
-    *
-    * @param string|null $id File id.
-    * @return \Cake\Http\Response|null Redirects to index.
-    * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-    */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $file = $this->Files->get($id);
-        if ($this->Files->delete($file)) {
-            $this->Flash->success(__('The file has been deleted.'));
-        } else {
-            $this->Flash->error(__('The file could not be deleted. Please, try again.'));
+            
+            $evidenceItems = $this->Files->EvidenceItems->find('list', ['limit' => 200]);
+            $this->set(compact('file', 'evidenceItems'));
+            $this->set('_serialize', ['file']);
         }
         
-        return $this->redirect(['action' => 'index']);
+        /**
+        * Delete method
+        *
+        * @param string|null $id File id.
+        * @return \Cake\Http\Response|null Redirects to index.
+        * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+        */
+        public function delete($id = null)
+        {
+            $this->request->allowMethod(['post', 'delete']);
+            $file = $this->Files->get($id);
+            if ($this->Files->delete($file)) {
+                $this->Flash->success(__('The file has been deleted.'));
+            } else {
+                $this->Flash->error(__('The file could not be deleted. Please, try again.'));
+            }
+            
+            return $this->redirect(['action' => 'index']);
+        }
     }
-}
