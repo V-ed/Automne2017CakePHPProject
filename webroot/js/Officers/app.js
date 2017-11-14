3cc27c85b75ada6c1e30464e175a65110c84203d
V-ed <guillaumemarcoux@gmail.com>
2017-11-14 16:27:11 -0500

    Started further work into linked lists

--- webroot/js/Officers/app.js
+++ webroot/js/Officers/app.js
@@ -1,6 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	
-	
+	$('#field-departments').change(function(event) {
+		
+		$rankField = $('#field-officer_ranks');
+		
+		var departmentId = $(this).val();
+		
+		if (departmentId) {
+			
+			$.ajax({
+				url: urlToLinkedListFilter,
+				data: 'category_id=' + departmentId,
+			})
+			.done(function(ranks) {
+				console.log("success");
+				
+				$rankField.find('option').remove();
+				
+				$.each(ranks, function (key, value)
+				{
+					$.each(value, function (childKey, childValue) {
+						$rankField.append('<option value=' + childValue.id + '>' + childValue.name + '</option>');
+					});
+				});
+				
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+			
+			
+		} else {
+			$('#subcategory-id').html('<option value="">Select Category first</option>');
+		}
+		
+	});
 	
 });
 
